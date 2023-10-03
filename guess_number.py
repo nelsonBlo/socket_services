1e7d0e77d4c74460e1ec4e80908daf4903a529ce
nelson_bocanegra_lopez <nelsonblopez@gmail.com>
2023-10-03 18:03:51 -0300

    wip

--- guess_number.py
+++ guess_number.py
@@ -14,15 +14,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
     (cli, add) = socket_server.accept()
     print(f"Client connected at: {add}")
 
-    cli.send(b"Try to guess my number! ")
+    cli.send(b"Try to guess my number [1-9]! \n")
+    cli.send(b"\rENTER or ESC to quit\n")
+
     while True:
         data = cli.recv(64).decode()
-        print(data)
+        print(f"{data}")
         if int(data) == number_to_guess:
-            cli.send(b"You made it!")
+            cli.send(b"\r\nYou made it!\n")
             break
         elif int(data) > number_to_guess:
-            cli.send(b"My number is less ")
+            cli.send(b"\r\nMy number is less \n")
         elif int(data) < number_to_guess:
-            cli.send(b"My number is higher ")
+            cli.send(b"\r\nMy number is higher \n")
     cli.close()
