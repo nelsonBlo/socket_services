36807ac5943d8d37fbc1fd05bf708460417bb8f1
nelson_bocanegra_lopez <nelsonblopez@gmail.com>
2023-10-06 13:06:05 -0300

    done with events also and some minor changes

--- guess_number_multi_client_events.py
+++ guess_number_multi_client_events.py
@@ -3,40 +3,42 @@ import random
 import threading
 
 number_to_guess = random.randrange(1, 9)
-stop_threads = False
 thread_name = ""
 
 
-def client_driver(cl, ad):
-    global stop_threads
+def client_driver(cl, ad, ev):
     global thread_name
-    data = f"Connected with {ad}\n"
-    cl.send(data.encode())
+    message = f"Connected with {ad}\n"
+    cl.send(message.encode())
     cl.send(b"Try to guess my number [1-9]! \n")
     cl.send(b"\rENTER or ESC to quit\n")
 
-    while True :
-        if stop_threads:
-            resolved_by = f"Already resolved by: {thread_name}\n"
-            cl.send(resolved_by.encode())
-            break
+    while not ev.is_set():
         data = cl.recv(64).decode()
         print(f"{data}")
         if int(data) == number_to_guess:
-            cl.send(b"\r\nYou made it!\n")
-            stop_threads = True
+            message = f"\r\n\nYOU MADE IT!! My number was {number_to_guess}\n"
+            cl.send(message.encode())
+            ev.set()
             thread_name = threading.current_thread().name
-            break
-        elif int(data) > number_to_guess:
+            cl.close()
+        elif int(data) > number_to_guess and not ev.is_set():
             cl.send(b"\r\nMy number is less \n")
-        elif int(data) < number_to_guess:
+        elif int(data) < number_to_guess and not ev.is_set():
             cl.send(b"\r\nMy number is higher \n")
-    cl.close()
+    try:
+        if ev.is_set():
+            resolved_by = f"\nThe number was {number_to_guess} and it was already resolved by: {thread_name}\n"
+            cl.send(resolved_by.encode())
+            cl.close()
+    except:
+        pass
 
 
 if __name__ == '__main__':
     IP = 'localhost'
     PORT = 2000
+    ev = threading.Event()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_server:
         socket_server.bind((IP, PORT))
         socket_server.listen()
@@ -47,6 +49,7 @@ if __name__ == '__main__':
             (cli, add) = socket_server.accept()
             # print(f"Client connected at: {add}")
             # new thread for each new connection
-            th = threading.Thread(target=client_driver, args=(cli, add))
+            th = threading.Thread(target=client_driver, args=(cli, add, ev))
             th.name = add
             th.start()
+            ev.clear()
